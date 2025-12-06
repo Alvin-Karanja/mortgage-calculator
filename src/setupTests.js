dcc56bbcd08b898ba607485206982a6936a42875
Alvin-Karanja <alvin2@proton.me>
2025-12-06 11:34:41 +0300

    test: mock canvas in setupTests and update App.test to current header

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,37 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Provide a minimal canvas context for Chart.js in the jsdom test environment
+// This prevents "Not implemented: HTMLCanvasElement.prototype.getContext" errors.
+if (typeof HTMLCanvasElement !== 'undefined') {
+	HTMLCanvasElement.prototype.getContext = function () {
+		return {
+			fillRect: () => {},
+			clearRect: () => {},
+			getImageData: (x, y, w, h) => ({ data: new Array(w * h * 4) }),
+			putImageData: () => {},
+			createImageData: () => [],
+			setTransform: () => {},
+			drawImage: () => {},
+			save: () => {},
+			fillText: () => {},
+			restore: () => {},
+			beginPath: () => {},
+			moveTo: () => {},
+			lineTo: () => {},
+			closePath: () => {},
+			stroke: () => {},
+			translate: () => {},
+			scale: () => {},
+			rotate: () => {},
+			arc: () => {},
+			fill: () => {},
+			measureText: () => ({ width: 0 }),
+			transform: () => {},
+			rect: () => {},
+			clip: () => {},
+			createLinearGradient: () => ({ addColorStop: () => {} }),
+		};
+	};
+}
